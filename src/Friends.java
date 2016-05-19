ffc86516206478c459523893b6a1c57fefa43cda
Revanty <league@WTS14.attlocal.net>
2016-05-18 20:01:58 -0700

    Added Turtle Pond and WIP Get Friends! :)

---
+++ src/Friends.java
@@ -0,0 +1,21 @@
+public class Friends {
+
+	public static void main(String[] args) {
+		// 3. Create an instance of Classmate for each person in your class.
+		// 4. set descriptions for each Classmate
+
+		// 5. Use your getter to print out each description
+	}
+
+}
+
+class Classmate {
+	String description;
+
+	// 1. create a setter that sets a description this person
+	void setDescription(String potato) {
+
+	}
+	// 2. create a getter that gets a description of this person
+
+}
