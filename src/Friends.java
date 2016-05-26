5414b518a0ac2b4f63e6e566b3ea8759f1e788eb
Revanty <league@WTS14.attlocal.net>
2016-05-25 19:59:04 -0700

    Finished Get Friends!

--- src/Friends.java
+++ src/Friends.java
@@ -2,20 +2,50 @@ public class Friends {
 
 	public static void main(String[] args) {
 		// 3. Create an instance of Classmate for each person in your class.
+
+		Classmate Bob = new Classmate();
+		Classmate Joe = new Classmate();
+		Classmate Fred = new Classmate();
+		Classmate Mikaela = new Classmate();
+		Classmate Kaiya = new Classmate();
+		Classmate Katherine = new Classmate();
+
 		// 4. set descriptions for each Classmate
 
+		Bob.setDescription("Bob is great.");
+		Joe.setDescription("Joe is fab.");
+		Fred.setDescription("Fred is awesome.");
+		Mikaela.setDescription("Mikaela is the best.");
+		Kaiya.setDescription("Kaiya is amazing.");
+		Katherine.setDescription("Katherine is super cool.");
+
 		// 5. Use your getter to print out each description
+
+		System.out.println(Bob.getDescription());
+		System.out.println(Joe.getDescription());
+		System.out.println(Fred.getDescription());
+		System.out.println(Mikaela.getDescription());
+		System.out.println(Kaiya.getDescription());
+		System.out.println(Katherine.getDescription());
+
 	}
 
 }
 
 class Classmate {
-	String description;
+	String potato;
 
 	// 1. create a setter that sets a description this person
 	void setDescription(String potato) {
 
-	}
-	// 2. create a getter that gets a description of this person
+		this.potato = potato;
 
+	}
+
+	// 2. create a getter that gets a description of this person
+	String getDescription() {
+
+		return (potato);
+
+	}
 }
